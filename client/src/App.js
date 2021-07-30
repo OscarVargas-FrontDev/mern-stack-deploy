676fdbd005589c92af8afe7966dcfbfc7c8cb922
Oscar Vargas <oscarprogrammer007@gmail.com>
2021-07-29 21:10:56 -0300

    inicio

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
 import Form from './components/Form';
 import DisplayUsers from './components/DisplayUsers';
 import axios from "axios";
@@ -34,29 +33,8 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/users'>users</Link>
-          </li>
-          <li>
-            <Link to='/about'>about</Link>
-          </li>
-        </ul>
-        <hr />
-        <Switch>
-          <Route exact path='/'>
-            <h1>Home</h1>
-          </Route>
-          <Route path='/users'>
-            <DisplayUsers users={this.state.users} />
-          </Route>
-          <Route path='/about'>
-            <Form addUser={this.addUser} />
-          </Route>
-        </Switch>
+        <Form addUser={this.addUser} /> 
+        <DisplayUsers users={this.state.users} />
       </div>
     );
   }
